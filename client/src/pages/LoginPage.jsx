2847d015076a0a2b90f6fc59150c176ecb791516
Linkoln <102604860+Linkoln195@users.noreply.github.com>
2024-07-03 14:58:21 +0200

    Update login link

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     const onFinish = (values) => {
         console.log('Success:', values);
         axios
-            .post('http://localhost:5000/login', values)
+            .post('https://mentoringtoolserver.onrender.com/login', values)
             .then(async (response) => {
                 response = response.data
                 await localStorage.setItem('mentorID', response);
